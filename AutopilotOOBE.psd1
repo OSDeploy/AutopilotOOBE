d58d64de92c9ca7c9d653ba11465ae968a37e0a6
David Segura <david@segura.org>
2023-07-15 11:16:13 -0500

    Revert "21.10.26.1"
    This reverts commit e818c3582de8f2730c996ec733fa2e508cfe5c6d.

--- AutopilotOOBE.psd1
+++ AutopilotOOBE.psd1
@@ -7,7 +7,7 @@
 RootModule = 'AutopilotOOBE.psm1'
 
 # Version number of his module.
-ModuleVersion = '21.10.26.1'
+ModuleVersion = '21.8.31.1'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
--- Project/MainWindow.ps1
+++ Project/MainWindow.ps1
@@ -374,6 +374,24 @@ if ($Global:AutopilotOOBE.AddToGroup) {
     $formMainWindowControlAddToGroupComboBox.Text = $Global:AutopilotOOBE.AddToGroup
 }
 #================================================
+#   AssignedUser Control
+#================================================
+# Disable the Control
+if ($Disabled -contains 'AssignedUser') {
+    $formMainWindowControlAssignedUserTextBox.IsEnabled = $false
+}
+
+# Hide the Control
+if ($Hidden -contains 'AssignedUser') {
+    $formMainWindowControlAssignedUserStackPanel.Visibility = 'Collapsed'
+}
+
+# Populate the Control
+$formMainWindowControlAssignedUserTextBox.Text = $Global:AutopilotOOBE.AssignedUserExample
+if ($Global:AutopilotOOBE.AssignedUser -gt 0) {
+    $formMainWindowControlAssignedUserTextBox.Text = $Global:AutopilotOOBE.AssignedUser
+}
+#================================================
 #   AssignedComputerName Control
 #================================================
 # Disable the Control
@@ -681,6 +699,10 @@ $formMainWindowControlRegisterButton.add_Click( {
 
     if ($formMainWindowControlGroupTagComboBox.Text -gt 0) {
         $Params.GroupTag = $formMainWindowControlGroupTagComboBox.Text
+    }
+
+    if (($formMainWindowControlAssignedUserTextBox.Text -gt 0) -and ($formMainWindowControlAssignedUserTextBox.Text -notmatch $Global:AutopilotOOBE.AssignedUserExample)) {
+        $Params.AssignedUser = $formMainWindowControlAssignedUserTextBox.Text
     }
 
     if (($formMainWindowControlAssignedComputerNameTextBox.Text -gt 0) -and ($formMainWindowControlAssignedComputerNameTextBox.Text -notmatch $Global:AutopilotOOBE.AssignedComputerNameExample)) {
